f5d61b96bc378b732381371d5487794fe821e491
Shanna-Tierney <60900942+Shanna-Tierney@users.noreply.github.com>
2020-02-11 15:19:08 -0600

    Image demo

---
+++ 8-image-demo/sketch.js
@@ -0,0 +1,23 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let pie;
+
+function preload() {
+  pie = loadImage("assets/pie.jpg")
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill("red");
+  // rect(mouseX, mouseY, 75, 75);
+  image(pie, mouseX, mouseY);
+}
