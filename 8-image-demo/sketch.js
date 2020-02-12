6df8cb32feb12ef50b2004430b7d7b11313974ca
Shanna-Tierney <60900942+Shanna-Tierney@users.noreply.github.com>
2020-02-12 14:58:41 -0600

    Finished image demo

--- 8-image-demo/sketch.js
+++ 8-image-demo/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let pie;
+let scalar = 1.0;
 
 function preload() {
   pie = loadImage("assets/pie.jpg")
@@ -19,5 +20,16 @@ function draw() {
   background(220);
   fill("red");
   // rect(mouseX, mouseY, 75, 75);
-  image(pie, mouseX, mouseY);
+  image(pie, mouseX, mouseY, scalar*pie.width, scalar*pie.height);
+  
 }
+
+function mouseWheel(event) {
+  print(event.delta);
+   if (event.delta <= 0) {
+     scalar *= 1.1;
+   }
+   else  {
+     scalar *= 0.9;
+   }
+ }
