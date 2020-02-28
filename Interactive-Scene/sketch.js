6e55b216a4c0f3a283c785983a304624ab5c2da6
Shanna-Tierney <60900942+Shanna-Tierney@users.noreply.github.com>
2020-02-28 15:19:39 -0600

    worked

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -9,6 +9,9 @@ let r;
 let g;
 let b;
 let a;
+let coin1;
+let coin2;
+let coin3;
 let coin1X;
 let coin1Y;
 let coin2X;
@@ -23,6 +26,8 @@ let movingUp = false;
 let movingDown = false;
 let movingLeft = false;
 let movingRight = false;
+let points = 0;
+let writer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -30,17 +35,23 @@ function setup() {
   g = random(0, 255);
   b = random(0, 255);
   a = random(0, 255);
+  coin1X = random(windowWidth, 0);
+  coin1Y =  random(0, windowHeight);
+  coin2X = random(windowWidth, 0);
+  coin2Y =  random(0, windowHeight);
+  coin3X = random(windowWidth, 0);
+  coin3Y =  random(0, windowHeight);
   ballX = width/2;
   ballY = height/2;
-  background(255);
-  placeCoins();
+  writer = createWriter("newFile.txt");
 }
 
 function draw() { 
-
+  background(255);
+  placeCoins();
   createBall();
   moveBall();
-  collectCoin();
+  youWin();
 }
 
 function createBall() {
@@ -50,31 +61,38 @@ function createBall() {
 
 // Creating coins for players to collect
 function placeCoins() {
-  coin1X = random(windowWidth, 0);
-  coin1Y = random(0, windowHeight);
   fill("gold");
-  ellipse(coin1X, coin1Y, 10, 10);
-  coin2X = random(windowWidth, 0);
-  coin2Y = random(0, windowHeight);
+  coin1 = ellipse(coin1X, coin1Y, 10, 10);
   fill("gold");
-  ellipse(coin2X, coin2Y, 10, 10);
-  coin3X = random(windowWidth, 0);
-  coin3Y = random(0, windowHeight);
+  coin2 = ellipse(coin2X, coin2Y, 10, 10);
   fill("gold");
-  ellipse(coin3X, coin3Y, 10, 10);
-
+  coin3 = ellipse(coin3X, coin3Y, 10, 10);
 }
 
 function createObstacals() {
 
 }
 
-function collectCoin() {
-  if(ballX - coinX <= 5 && ballY - coinY <= 5) {
-    let point;
-    point ++;
-    print(point);
+function collectCoin1() {
+  if(ballX - coin1X < 5 && ballY - coin1Y < 5)
+}
+
+function youWin() {
+  if(points === 3) {
+    writer.write(["YOU WIN, CONGRATS!"]);
+    document.location.reload();
+    writer.close();
   }
+}
+
+
+function collectCoin() {
+  if(ballX - coin1X <= 5 && ballY - coin1Y <= 5){
+    print(points);
+    return points ++;
+  }
+
+
 }
 
 // Making  the ball move with WASD keys 
